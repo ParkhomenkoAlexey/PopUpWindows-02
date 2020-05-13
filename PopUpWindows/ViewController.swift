b108f4ae46ed8605bc32eb4904aa1205ea920ec0
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-05-13 22:53:12 +0500

    Starter Project

--- PopUpWindows/ViewController.swift
+++ PopUpWindows/ViewController.swift
@@ -9,12 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let popUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .white
+        button.setTitle("Show Pop Up View", for: .normal)
+        button.setTitleColor(UIColor.systemIndigo, for: .normal)
+        button.addTarget(self, action: #selector(handleShowPopUp), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    let alertButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .white
+        button.setTitle("Show Alert View", for: .normal)
+        button.setTitleColor(UIColor.systemIndigo, for: .normal)
+        button.addTarget(self, action: #selector(handleShowAlert), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 5
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupConstraints()
+    }
+    
+    @objc func handleShowPopUp() {
+        print(#function)
+    }
+    
+    @objc func handleShowAlert() {
+        print(#function)
     }
 
 
+}
+
+// MARK: - Setup Constraints
+extension ViewController {
+    func setupConstraints() {
+        view.addSubview(popUpButton)
+        view.addSubview(alertButton)
+        
+        NSLayoutConstraint.activate([
+            popUpButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            popUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            popUpButton.heightAnchor.constraint(equalToConstant: 50),
+            popUpButton.widthAnchor.constraint(equalToConstant: view.frame.width - 32)
+        ])
+        
+        NSLayoutConstraint.activate([
+            alertButton.topAnchor.constraint(equalTo: popUpButton.bottomAnchor, constant: 32),
+            alertButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            alertButton.heightAnchor.constraint(equalToConstant: 50),
+            alertButton.widthAnchor.constraint(equalToConstant: view.frame.width - 32)
+        ])
+    }
 }
 
